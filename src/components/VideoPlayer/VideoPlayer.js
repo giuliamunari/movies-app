3edccbad69615d9d232632901bf3e4e58e3382f5
Astrid88 <giu.munari@gmail.com>
2019-08-18 12:24:50 +0200

    tried with replay

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -3,10 +3,7 @@ import React from 'react'
 export default function VideoPlayer(props) {
     return (
         <div>
-            <video width="400" controls>
-                <source src="https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8​" type="video/m3u8​" />
-                Your browser does not support HTML5 video.
-            </video>
+           
         </div>
     )
 }
