26ec1695fb05ad69ee8c9c8e0b41fed61af11a3c
Astrid88 <giu.munari@gmail.com>
2019-08-18 18:52:43 +0200

    add mux

--- src/components/VideoPlayer/VideoPlayerContainer.js
+++ src/components/VideoPlayer/VideoPlayerContainer.js
@@ -2,43 +2,39 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Replay } from 'vimond-replay';
 import 'vimond-replay/index.css';
-import CompoundVideoStreamer from 'vimond-replay/video-streamer/compound';
+//import muxjs from 'mux'
+import ShakaVideoStreamer from 'vimond-replay/video-streamer/shaka-player';
 
 class VideoPlayerContainer extends Component {
-
+	
     id = parseInt(this.props.match.params.id)
-    type = this.props.match.params.type
+    //type = this.props.match.params.type
     componentDidMount() {
-
+		//window.muxjs = muxjs
     }
     replayOptions = {
-        videoStreamer: {
-            hlsjs: {
-                customConfiguration: {
-                    capLevelToPlayerSize: true,
-                    maxBufferLength: 45
-                }
-            },
-            shaka: {
-                customConfiguration: {
-                    streaming: {
-                        bufferingGoal: 120
-                    }
-                }
-            }
-        }
-    };
-
+		videoStreamer: {
+		  shaka: {
+			customConfiguration: {
+			  streaming: {
+				bufferingGoal: 120
+			  }
+			}
+		  }
+		}
+	  };
     render() {
+		 
         return (
             <div>
                 <Replay
                     source={{
                         streamUrl: 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8',
                         contentType: 'application/x-mpegurl',
-                    }}
+					}}
+					options={this.replayOptions}>
                 >
-                    <CompoundVideoStreamer />
+                     <ShakaVideoStreamer />
                 </Replay>
             </div>
         )
