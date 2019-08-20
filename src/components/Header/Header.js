b94f35e79b478a9f8d81450cb9905a532f5ef623
Astrid88 <giu.munari@gmail.com>
2019-08-20 10:21:18 +0200

    add basic styles for dashboard

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className='header'>
+                <h1>Welcome to the Streaming app</h1>
+                <h2>Enjoy the vision</h2>
+            </div>
+        )
+    }
+}
